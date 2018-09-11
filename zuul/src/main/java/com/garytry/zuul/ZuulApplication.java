626c7a15bf526bb06907e3abf5c80535208a9869
Garytry <18210323686@163.com>
2018-09-11 19:55:27 +0800

    服务提供者和服务消费者基础代码提交

--- zuul/src/main/java/com/garytry/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/garytry/zuul/ZuulApplication.java
@@ -2,8 +2,12 @@ package com.garytry.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
 public class ZuulApplication {
 
     public static void main(String[] args) {
